427813cc185bc019cb39f527637fbebe443308f6
noob9590 <dors9590@gmail.com>
2020-05-13 20:06:53 +0300

    last master commit
    Added firebase Dependancy, also firebase initialize code to the index.js.
    In addition, a folder named components is created, please create all the new components overthere.

--- Massa React/src/index.js
+++ Massa React/src/index.js
@@ -5,8 +5,23 @@ import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import "./index.css";
 import App from "./App";
-
+import * as firebase from 'firebase'
+import "firebase/firestore";
 import registerServiceWorker from './registerServiceWorker';
+
+const config = {
+    apiKey: "AIzaSyAInRpKsKV1c9wfn93jjCLuSW9eXjWqmTI",
+    authDomain: "socialtrip-f5408.firebaseapp.com",
+    databaseURL: "https://socialtrip-f5408.firebaseio.com",
+    projectId: "socialtrip-f5408",
+    storageBucket: "socialtrip-f5408.appspot.com",
+    messagingSenderId: "711929649987",
+    appId: "1:711929649987:web:ac989f29358f72b1b9adf6",
+    measurementId: "G-RMJ74WB7DJ"
+  };
+  firebase.initializeApp(config)
+  
+
 
 ReactDOM.render( <App /> , document.getElementById('root'));
 

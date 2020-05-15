c4e247e0f1b54a7931cca70ab3f74ff08b0f81d8
gal shalom <galshalom@h-MacBook-Pro-sl-gal.local>
2020-05-15 12:43:01 +0300

    Start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FullPageIntroWithFixedTransparentNavbar from './mainPage'
+function App() {
+  return (
+    <div className="App">
+      <FullPageIntroWithFixedTransparentNavbar/>
+    </div>
+  );
+}
+
+export default App;
+
